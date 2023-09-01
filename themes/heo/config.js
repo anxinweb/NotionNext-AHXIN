afe6ca058ad428c97bf60bac973599cb7cb23894
fangs <45865656+anxinweb@users.noreply.github.com>
2023-09-01 15:32:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://ahxin.cn/about' },
+    { title: '我是ah新呀，欢迎来到我的博客', url: 'https://ahxin.cn/about' },
     { title: '', url: '' }
   ],
 
